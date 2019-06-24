274d58b0657e69e0979a8ed37b24fdf34f915d15
18581023664 <451769267@qq.com>
2019-06-24 12:25:06 +0800

    '项目结构搭建好,到header显示为止'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    hello
+    <m-header></m-header>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import MHeader from 'components/m-header/m-header'
+
+  export default {
+    name: 'App',
+    components:{
+      MHeader
+    }
+  }
 </script>
 
 <style lang="stylus" scoped>
-  @import "~common/stylus/variable.styl"
-
 </style>
